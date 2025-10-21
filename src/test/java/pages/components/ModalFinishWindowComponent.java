40b87131fb4c640cc2d6ff2b8e7a72c85d7a3b9e
alexaastri <AVoronin@lanit.ru>
2025-10-21 16:15:19 +0300

    finish table's css locator was moved to a separate selenide element

--- src/test/java/pages/components/ModalFinishWindowComponent.java
+++ src/test/java/pages/components/ModalFinishWindowComponent.java
@@ -1,13 +1,17 @@
 package pages.components;
 
+import com.codeborne.selenide.SelenideElement;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
 public class ModalFinishWindowComponent {
 
+    private final SelenideElement finishTable = $(".table-responsive");
+
     public ModalFinishWindowComponent checkModalFinishWindow(String key, String value) {
-        $(".table-responsive").$(byText(key)).parent().shouldHave(text(value));
+        $(finishTable).$(byText(key)).parent().shouldHave(text(value));
         return this;
     }
 }
