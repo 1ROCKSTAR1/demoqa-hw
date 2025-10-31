f0fb6fd35444cc5a37003b951174e17f7e97a4dd
alexaastri <AVoronin@lanit.ru>
2025-10-31 09:50:55 +0300

    beforeEach was added and allureListener was moved in there

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class BaseTest {
 
@@ -13,6 +14,10 @@ public class BaseTest {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 5000;
+    }
+
+    @BeforeEach
+    void beforeSingle() {
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
 }
