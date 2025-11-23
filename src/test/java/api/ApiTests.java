c2e2b4d8341054d31ae701fa5c1e5fe7714609e6
Alex299 <elrockstar25@gmail.com>
2025-11-23 22:07:24 +0300

    BeforeAll for api was added

--- src/test/java/api/ApiTests.java
+++ src/test/java/api/ApiTests.java
@@ -1,20 +1,33 @@
 package api;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.http.Header;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.emptyString;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.matchesPattern;
 
 public class ApiTests {
+
+    @BeforeAll
+    public static void setUp() {
+        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.basePath = "/api";
+    }
 
     Header header = new Header("x-api-key", "reqres-free-v1");
 
     @Test
+    @DisplayName("Создать пользователя")
     public void createUserTest() {
         UserCreateData newUser = new UserCreateData("Tom","cleaner");
 
@@ -25,14 +38,18 @@ public class ApiTests {
                 .body(newUser)
                 .contentType(ContentType.JSON)
                 .when()
-                .post("https://reqres.in/api/users")
+                .post(baseURI + basePath + "/users")
                 .then()
                 .log().status()
                 .log().body()
-                .statusCode(201);
+                .statusCode(201)
+                .body("name",equalTo("Tom"))
+                .body("job",equalTo("cleaner"))
+                .body("id", notNullValue());
     }
 
     @Test
+    @DisplayName("НЕГАТИВНЫЙ ТЕСТ. Создание пользователя без должности.")
     public void createFailUserTest() {
         String newNegativeUser = "{ \"name\": \"Tom\"}";
 
@@ -43,14 +60,16 @@ public class ApiTests {
                 .body(newNegativeUser)
                 .contentType(ContentType.JSON)
                 .when()
-                .post("https://reqres.in/api/users")
+                .post(baseURI + basePath + "/users")
                 .then()
                 .log().status()
                 .log().body()
-                .statusCode(400);
+                .statusCode(400)
+                .body("message",equalTo("Missing job"));
     }
 
     @Test
+    @DisplayName("Получить пользователя по id")
     public void getOneUserTest() {
         given()
                 .log().uri()
@@ -58,14 +77,18 @@ public class ApiTests {
                 .log().headers()
                 .contentType(ContentType.JSON)
                 .when()
-                .get("https://reqres.in/api/users/2")
+                .get(baseURI + basePath + "/users/2")
                 .then()
                 .log().status()
                 .log().body()
-                .statusCode(200);
+                .statusCode(200)
+                .body("data.id", notNullValue())
+                .body("data.email",endsWith("@reqres.in"))
+                .body("data.avatar", endsWith(".jpg"));
     }
 
     @Test
+    @DisplayName("Авторизация пользователя")
     public void successfulLoginTest() {
 
         UserLoginData user = new UserLoginData("eve.holt@reqres.in","cityslicka");
@@ -77,15 +100,19 @@ public class ApiTests {
                 .body(user)
                 .contentType(ContentType.JSON)
                 .when()
-                .post("https://reqres.in/api/login")
+                .post(baseURI + basePath + "/login")
                 .then()
                 .log().status()
                 .log().body()
                 .statusCode(200)
-                .body("token", is("QpwL5tke4Pnpja7X4"));
+                .body("token", notNullValue())
+                .body("token", not(emptyString()))
+                .body("token.length()", greaterThan(10))
+                .body("token", matchesPattern("^[a-zA-Z0-9]+$"));
     }
 
     @Test
+    @DisplayName("Получение списка пользователей")
     public void getAllUsersTest() {
 
         List<UserResponse> users = given().contentType(ContentType.JSON)
@@ -93,7 +120,7 @@ public class ApiTests {
                 .header(header)
                 .log().headers()
                 .when()
-                .get("https://reqres.in/api/users?page=2")
+                .get(baseURI + basePath + "/users?page=2")
                 .then()
                 .log().body()
                 .extract().jsonPath()
