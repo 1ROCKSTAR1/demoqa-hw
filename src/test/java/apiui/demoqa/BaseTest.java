8628b1f51eb783a5c7a604c380df699902f7b703
Alex_299 <elrockstar25@gmail.com>
2025-12-12 20:50:49 +0300

    selenoid was added to basetest

--- src/test/java/apiui/demoqa/BaseTest.java
+++ src/test/java/apiui/demoqa/BaseTest.java
@@ -2,12 +2,14 @@ package apiui.demoqa;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public abstract class BaseTest {
 
@@ -18,11 +20,19 @@ public abstract class BaseTest {
 
     @BeforeAll
     static void setUp() {
-        Configuration.baseUrl = "https://demoqa.com";
-        RestAssured.baseURI = "https://demoqa.com";
-        Configuration.browserSize = "1920x1200";
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 5000;
+        Configuration.baseUrl = System.getProperty("baseUrl","https://demoqa.com");
+        Configuration.browser = System.getProperty("browser","chrome");
+        Configuration.browserSize = System.getProperty("resolution","1920x1080");
+        Configuration.browserVersion = System.getProperty("browserVersion","127.0");
+        Configuration.timeout = 10000;
+        Configuration.remote = System.getProperty("remoteDriverUrl","https://user1:1234@selenoid.autotests.cloud/wd/hub");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
 
         requestSpecification = new RequestSpecBuilder()
                 .setBaseUri("https://demoqa.com")
